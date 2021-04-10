437af2c8e910d9779d692ba8ae2ed79bd68753b3
Oliver <rift95@live.se>
2021-04-10 20:54:55 +0200

    improved markdown redering

--- app/src/components/RenderMarkdown.tsx
+++ app/src/components/RenderMarkdown.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
 import gfm from 'remark-gfm';
+import html from 'remark-html';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    document: {
+      '& a': {
+        color: theme.palette.primary.main,
+        textDecoration: 'none',
+      },
+      '& a:hover': {
+        textDecoration: 'underline',
+      },
+    },
+  })
+);
 
 interface Props {
   markdown: string;
 }
 
 export const RenderMarkdown = (props: Props) => {
-  return <ReactMarkdown plugins={[gfm]} children={props.markdown} />;
+  const classes = useStyles();
+
+  return (
+    <ReactMarkdown
+      className={classes.document}
+      linkTarget="_blank"
+      transformImageUri={(uri) => {
+        console.log('Image', uri);
+        return uri;
+      }}
+      transformLinkUri={(uri) => {
+        console.log('Link', uri);
+        return uri;
+      }}
+      plugins={[gfm, html]}
+      children={props.markdown}
+    />
+  );
 };
